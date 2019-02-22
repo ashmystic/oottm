002c460642eacc816f37cc632f54f8e38a6298ee
Andrew Herman <andrew.scott.herman@gmail.com>
2019-02-22 09:11:39 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# oottm
-Interactive Oracle Card Set based on the Order of the Twelve Mushrooms Vision
+# OOTTM ("Order of the Twelve Mushrooms")
+Interactive Oracle Card Set based on the Order of the Twelve Mushrooms Vision by Ash Mystic
+
+Live site at aherman.github.io/oottm
---
+++ javascript/script.js
@@ -0,0 +1,172 @@
+$(function() {
+
+  var carousel = document.querySelector('.carousel');
+  var cells = carousel.querySelectorAll('.carousel__cell');
+  var cellCount = 7;
+  var selectedIndex = 0;
+  var cellWidth = carousel.offsetWidth;
+  var cellHeight = carousel.offsetHeight;
+  var isHorizontal = true;
+  var scope = 'Stages';
+  var rotateFn = 'rotateY';
+  var radius, theta;
+  // console.log( cellWidth, cellHeight );
+  var startX, startY;
+  var deltaX, deltaY;
+
+  function rotateCarousel() {
+    var angle = theta * selectedIndex * -1;
+    carousel.style.transform = 'translateZ(' + -radius + 'px) ' + 
+      rotateFn + '(' + angle + 'deg)';
+  }
+
+/*
+  carousel.addEventListener( 'dragstart', function(event) {
+ //   console.log(event.screenX);
+    startX = event.screenX;
+    startY = event.screenY;
+  });
+  carousel.addEventListener( 'drag', function(event) {
+    deltaX = event.screenX - startX;
+    deltaY = event.screenY - startY;
+    console.log(deltaX);
+    selectedIndex += Math.floor(deltaX / 500);
+  //  selectedIndex = (deltaX < 0) ? -selectedIndex : selectedIndex;
+    console.log(selectedIndex);
+    rotateCarousel();
+  });
+*/
+
+  var prevButton = document.querySelector('.previous-button');
+  prevButton.addEventListener( 'click', function() {
+    selectedIndex--;
+    rotateCarousel();
+  });
+
+  var nextButton = document.querySelector('.next-button');
+  nextButton.addEventListener( 'click', function() {
+    selectedIndex++;
+    rotateCarousel();
+  });
+
+  var flipButton = document.querySelector('.flip-button');
+  flipButton.addEventListener( 'click', function() {
+    flipCarousel();
+  });
+
+  var spinButton = document.querySelector('.spin-button');
+  spinButton.addEventListener( 'click', function() {
+    selectedIndex += Math.floor(Math.random()*cellCount)+1;
+    rotateCarousel();
+    if(Math.random() >= 0.5)
+      flipCarousel();
+  });
+
+
+  function flipCarousel() {
+    var cards = document.querySelectorAll('.card');
+    for ( var i=0; i < cards.length; i++ ) {
+      cards[i].classList.toggle('is-flipped');
+    }
+    var cardFront = document.querySelectorAll('.card__face--front');
+    for ( var i=0; i < cardFront.length; i++ ) {
+    //  cardFront[i].style.setAttribute ("display", "none");
+    // cardFront[i].style.setProperty ("display", "none", null);
+    }
+    scope = (scope == 'Stages') ? 'States' : 'Stages';
+    var scopeText = document.querySelector('.scope-text');
+    scopeText.innerHTML = scope;
+
+  }
+
+
+  function changeCarousel() {
+    theta = 360 / cellCount;
+    var cellSize = isHorizontal ? cellWidth : cellHeight;
+    radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellCount ) );
+    for ( var i=0; i < cells.length; i++ ) {
+      var cell = cells[i];
+      if ( i < cellCount ) {
+        // visible cell
+        cell.style.opacity = 1;
+        var cellAngle = theta * i;
+        cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
+      } else {
+        // hidden cell
+        cell.style.opacity = 0;
+        cell.style.transform = 'none';
+      }
+    }
+
+    rotateCarousel();
+  }
+
+  // set initials
+  changeCarousel();
+
+  /*
+  Buttons
+  */
+  // var buttonGroup = document.querySelector('.button-group');
+  // buttonGroup.addEventListener( 'click', function() {
+  //     var cards = document.querySelectorAll('.card');
+  //     for ( var i=0; i < cards.length; i++ ) {
+  //       cards[i].classList.toggle('is-flipped');
+  //     }
+  //   });
+
+  // buttonGroup.each(function(i, buttonGroup) {
+  //       var $buttonGroup = $(buttonGroup);
+  //       buttonGroup.on('click', 'button', function() {
+  //           buttonGroup.find('.is-checked').removeClass('is-checked');
+  //           $(this).addClass('is-checked');
+  //           cards[i].classList.toggle('is-flipped');
+  //       });
+  //   });
+
+  /*
+  Dialog
+  */
+  // Show dialog when card is clicked
+  $('.card__face').click(function(event) {
+      var modalId = $(this).attr('modal');
+      // Workaround for not being able to differentiate clicks on front vs back
+      var modalIdList = [
+        {1: "modal-birth", 2: "modal-growth", 3: "modal-relationships", 4: "modal-aging", 5: "modal-loss", 6: "modal-death", 7: "modal-tree"},
+        {1: "modal-presence", 2: "modal-gratitude", 3: "modal-synchronicity", 4: "modal-gnosis", 5: "modal-meta-gnosis", 6: "modal-rapture", 7: "modal-spirit"}
+      ];
+      if(scope == "Stages") {
+        showDialog(modalIdList[0][modalId]); 
+      } else {
+        showDialog(modalIdList[1][modalId]);
+      }
+    });
+
+  showDialog = function (modalId) {
+    if(modalId) {
+      modal = document.getElementById(modalId);
+      modal.style.display = "block";
+      
+      var span = modal.getElementsByClassName("close")[0];
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+
+      $('.dialog-link').click(function(event) {
+        event.preventDefault();
+        modal.style.display = "none";
+        
+        var modalId = $(this).attr('modal');
+        showDialog(modalId); 
+      });
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    }
+  }
+
+});
